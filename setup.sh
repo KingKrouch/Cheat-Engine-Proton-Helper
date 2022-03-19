d0b41b7d4df804ee3235046d5ce01a39174455ed
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2022-03-19 18:29:41 -0400

    Update setup to use Cheat Engine 7.4.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
  #!/bin/sh
 
-wget https://d3u58y6epi0j3h.cloudfront.net/installer/7850326519215488/01871948 -O CheatEngine72.exe # Downloads the Cheat Engine installer from the site.
-wine CheatEngine72.exe # Runs the Cheat Engine installer in Wine.
+wget https://github.com/cheat-engine/cheat-engine/releases/download/7.4/CheatEngine74.exe -O CheatEngine74.exe # Downloads the Cheat Engine installer from the site.
+wine CheatEngine74.exe # Runs the Cheat Engine installer in Wine.
 chmod +x ./CE.sh # Makes the Cheat Engine script for running in Proton executable. If this does nothing, then chmod it manually.
-rm CheatEngine72.exe # Deletes the CheatEngine installer file.
+rm CheatEngine74.exe # Deletes the CheatEngine installer file.
